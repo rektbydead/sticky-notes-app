9ed69d4948d7b9a84fa468c0c0d56781c9d6a2b5
Ruben Lousada <ruben.lousada.dev@gmail.com>
2025-12-05 16:25:16 +0000

    Frontend initial commit

---
+++ frontend/electron/main.js
@@ -0,0 +1,26 @@
+import { app, BrowserWindow, Menu } from "electron"
+import path from "path"
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+async function main() {
+    const window = new BrowserWindow({
+		webPreferences: { preload: path.join(__dirname, 'preload.js'), contextIsolation: false },
+		width: 1280, height: 720,
+
+		titleBarOverlay: {
+		  symbolColor: '#FFF',
+		  color: '#2b252c',
+		  height: 10
+		}
+	})
+
+    Menu.setApplicationMenu(null);
+    await window.loadURL("http://localhost:5173")
+}
+
+app.whenReady()
+    .then(main)
