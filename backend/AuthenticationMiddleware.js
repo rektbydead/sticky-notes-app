c2b157b9a5c59ffd3fde9c52fd9d1f973090d27c
Ruben Lousada <ruben.lousada.dev@gmail.com>
2025-12-20 12:22:15 +0000

    remove old backend & rename new

--- backend/AuthenticationMiddleware.js
+++
@@ -1,7 +0,0 @@
-const isAuthenticated = async (req, res, next) => {
-    return !req.session?.userId
-        ? res.status(401).json({ error: 'Authentication required' })
-        : next()
-};
-
-module.exports = isAuthenticated;
