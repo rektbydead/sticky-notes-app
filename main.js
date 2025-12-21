6594427b5855cd3b74c60ec229c6011f17c0750b
Vito Carvalhais <vito@student.dei.uc.pt>
2025-12-21 12:33:05 +0000

    add favicon and folder ubication.

--- main.js
+++ main.js
@@ -4,18 +4,19 @@ const path = require('path')
 
 async function main() {
     const window = new BrowserWindow({
-		// webPreferences: { preload: path.join(__dirname, 'preload.js'), contextIsolation: false },
-		width: 1920,
+        // webPreferences: { preload: path.join(__dirname, 'preload.js'), contextIsolation: false },
+        icon: path.join(__dirname, './frontend/favicon.ico'),
+        width: 1920,
         height: 1080,
         minWidth: 1280,
         minHeight: 800,
 
-		titleBarOverlay: {
-		  symbolColor: '#FFF',
-		  color: '#2b252c',
-		  height: 10
-		}
-	})
+        titleBarOverlay: {
+          symbolColor: '#FFF',
+          color: '#2b252c',
+          height: 10
+        }
+    })
 
     Menu.setApplicationMenu(null)
     window.loadURL("http://web-dev-grupo01.dei.uc.pt/")
